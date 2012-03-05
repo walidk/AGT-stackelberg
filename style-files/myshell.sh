578f15db0395feda5dd3eb16e136d6cbccdf3f72
Jack Reilly <jackdreilly@gmail.com>
2012-03-04 18:36:40 -0800

    clean up

--- style-files/myshell.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-CONF_FILE=$(grep IEEEconf ~/Library/Application\ Support/LyX-2.0/clsFiles.lst -m 1)
-mv $CONF_FILE $CONF_FILE".old"
-wget "http://css.paperplaza.net/conferences/support/files/ieeeconf.cls" -O $CONF_FILE
-echo "now go to lyx, and click Lyx->Reconfigure, then restart. Then change class to article (IEEEconf)"
